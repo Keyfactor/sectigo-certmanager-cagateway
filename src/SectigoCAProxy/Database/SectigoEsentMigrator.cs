59337442ea38493fdadf96a775460d01d2273fb2
Gary Noe <5043505+gnoeou@users.noreply.github.com>
2021-05-11 14:39:20 -0400

    1.0.3: Update workflow, Readme, CertType, and DbMigrator

---
+++ src/SectigoCAProxy/Database/SectigoEsentMigrator.cs
@@ -0,0 +1,18 @@
+﻿using CAProxy.AnyGateway.Interfaces;
+using CAProxy.AnyGateway.Models;
+using CSS.Common.Logging;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Isam.Esent.Collections.Generic;
+using CAProxy.Data.CAProxyDAL;
+using System.IO;
+using CAProxy.Data.CAProxyDAL.Esent;
+
+namespace Keyfactor.AnyGateway.Sectigo.Database
+{
+    
+}
---
+++ src/SectigoEsentMigrator/SectigoEsentMigrator.cs
@@ -0,0 +1,73 @@
+﻿using CAProxy.AnyGateway.Interfaces;
+using CAProxy.AnyGateway.Models;
+using CAProxy.Data.CAProxyDAL.Esent;
+using CSS.Common.Logging;
+using System;
+using System.Collections.Concurrent;
+using System.IO;
+
+namespace Keyfactor.AnyGateway.Sectigo.Database
+{
+    public class SectigoEsentMigrator : LoggingClientBase, IDatabaseMigrator
+    {
+        public void GetAllCertificates(BlockingCollection<DBCertificate> certificateBuffer, DBCertificateAuthority certificateAuthority, string esentDatabasePath)
+        {
+            const string ESENT_PATH = "C:\\ProgramData\\CertSvcProxy\\TemplateDB\\CAProxyComodoCCM.edb";
+            try
+            {
+                if (!string.IsNullOrEmpty(esentDatabasePath))
+                {
+                    Logger.Warn($"--esent-path parameter is not supported.  Using default {ESENT_PATH}");
+                }
+
+                Logger.Trace("Getting All Certificates");
+                if (File.Exists(ESENT_PATH))
+                {
+                    ComodoCcmESENTConnector conn = new ComodoCcmESENTConnector();
+                    foreach (var record in conn.GetAllRecords())
+                    {
+                        var arrayCert = record.Certificate?.Split(new string[] { "-----" }, StringSplitOptions.RemoveEmptyEntries);
+                        var arrayCsr = record.CSR?.Split(new string[] { "-----" }, StringSplitOptions.RemoveEmptyEntries);
+
+                        string pemCert = string.Empty;
+                        if (arrayCert?.Length > 1)
+                            pemCert = arrayCert[1].Replace("\n", string.Empty);
+
+                        string pemCsr = string.Empty;
+                        if (arrayCsr?.Length > 1)
+                            pemCsr = arrayCsr[1].Replace("\n", string.Empty);
+
+                        DBCertificate dbCert = new DBCertificate
+                        {
+                            CARequestID = $"{record.RequestID}",
+                            Template = record.CertificateTemplate,
+                            SubmissionDate = record.SubmitDate, // Default input for migration. Will be updated on sync. Date must be between 1/1/1753 12:00:00 AM and 12/31/9999 11:59:59 PM
+                            Status = 0, // Default input for migration. Will be updated on sync
+                            CertificateAuthorityId = certificateAuthority.Id,
+                            Certificate = Convert.FromBase64String(pemCert),
+                            CSR = Convert.FromBase64String(pemCsr),
+                            Requester = record.Requestor,
+                            IssuedCN = record.CommonName
+                        };
+                        certificateBuffer.Add(dbCert);
+
+                    }
+                }
+                else
+                {
+                    throw new Exception($"ESENT DB at C:\\ProgramData\\CertSvcProxy\\TemplateDB\\CAProxyComodoCCM.edb not found. Migration failed.");
+                }
+            }
+            catch (NullReferenceException nullex)
+            {
+                Logger.Error(nullex);
+                throw;
+            }
+            catch (Exception ex)
+            {
+                Logger.Error(ex);
+                throw;
+            }
+        }
+    }
+}
